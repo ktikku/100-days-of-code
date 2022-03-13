84fb1aa9540f6607c268cb819750b62eec89634c
ktikku <tikku_karan@lilly.com>
2022-03-13 20:47:21 +0530

    Day 14 Prob 1

---
+++ move-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let zero = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      swap(nums, i, zero);
+      zero++;
+    }
+  }
+  return nums;
+};
+
+var swap = function (nums, a, b) {
+  const tmp = nums[a];
+  nums[a] = nums[b];
+  nums[b] = tmp;
+}
