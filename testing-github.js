70655a35c9c7c38ee50256e733db8e05f9668ce0
Karan Tikku <50620678+ktikku@users.noreply.github.com>
2023-09-08 23:13:29 +0530

    Update testing-github.js

--- testing-github.js
+++ testing-github.js
@@ -1,3 +1,5 @@
-console.log('testing wqewqe vs merge')
-console.log('Hello varun')
+console.log('This is a new test')
+console.log('This is vue')
+console.log('This is a new test')
+console.log('This is a new test')
 console.log('This is a new test')
